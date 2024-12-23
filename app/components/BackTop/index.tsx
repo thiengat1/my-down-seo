759cf36c1f4e3ccb237fccde1b24f19e79d9e20f
admin <admin@Mac-mini.local>
2024-12-23 18:16:40 +0700

    fix: fix some bug

--- app/components/BackTop/index.tsx
+++ app/components/BackTop/index.tsx
@@ -1,17 +1,11 @@
 'use client';
 import { useState, useEffect } from 'react';
-import { animated, useSpring } from '@react-spring/web';
 
 export interface IBackTopProps {}
 
 export default function BackTop(props: IBackTopProps) {
   const [isVisible, setIsVisible] = useState<boolean>(false);
-  const animation = useSpring({
-    transform: 'translateY(0)',
-    from: { transform: 'translateY(20px)' },
-    config: { tension: 300, friction: 10 },
-    loop: { reverse: true },
-  });
+
   const handleDisplayBackTop = () => {
     if (window.pageYOffset >= window.innerHeight) {
       setIsVisible(true);
@@ -31,13 +25,9 @@ export default function BackTop(props: IBackTopProps) {
       window.removeEventListener('scroll', handleDisplayBackTop);
     };
   }, []);
-  const CustomAnimatedDiv = ({ children, ...props }: any) => {
-    return <animated.div {...props}>{children}</animated.div>;
-  };
   if (isVisible)
     return (
-      <CustomAnimatedDiv
-        style={animation}
+      <div
         className='fixed bottom-12 right-12 w-10 h-10 lg:w-12 lg:h-12 rounded-full border-2 border-slate-400  border-solid hover:opacity-70 cursor-pointer'
         onClick={scrollToTop}
       >
@@ -46,7 +36,7 @@ export default function BackTop(props: IBackTopProps) {
           alt='back-to-top'
           className='object-contain w-full h-full'
         />
-      </CustomAnimatedDiv>
+      </div>
     );
   return null;
 }
